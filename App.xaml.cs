9b1e32cd90a4237dc28a2d8cb289120a8453a978
Cyril Ologho <cyrielo911@gmail.com>
2023-07-13 17:59:24 -0400

    reset project

--- App.xaml.cs
+++ App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace Assignmen_4
+namespace ui_asg4
 {
     /// <summary>
     /// Interaction logic for App.xaml
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Assignmen_4
+namespace ui_asg4
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -23,7 +23,6 @@ namespace Assignmen_4
         public MainWindow()
         {
             InitializeComponent();
-            
         }
     }
 }
