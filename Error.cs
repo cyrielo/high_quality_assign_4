c998fbe3ebd812469238d4b1eb4ccf1e12332974
Cyril Ologho <cyrielo911@gmail.com>
2023-07-14 13:37:38 -0400

    add error class

---
+++ Error.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace ui_asg4
+{
+    public partial class TextBoxError
+    {
+        private string message = "";
+        private TextBox textBox = new TextBox();
+        public string Message { get => message; set => message = value; }
+        public TextBox TextBox { get => textBox; set => textBox = value; }
+    }
+}
